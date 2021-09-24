2da2e4d05574a13e675b4264c23d6898e52c0439
Joris Nijkamp <JGW.Nijkamp@student.han.nl>
2021-09-24 12:14:59 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+//LOCALHOST DEVELOPMENT
+const USERNAME = "";
+const PASSWORD = "";
+const HOST = "localhost";
+const PORT = "27017";
+const DB = "quizzer";
+
+var exports = module.exports = {USERNAME, PASSWORD, HOST, PORT, DB};
