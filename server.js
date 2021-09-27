33dd4630546784efd2d533810705f98033054bac
Joris Nijkamp <JGW.Nijkamp@student.han.nl>
2021-09-27 13:37:28 +0200

    Server changes voor CORS policy

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
     origin: true,
     credentials: true,
-    allowedHeaders: "*",
+    allowedHeaders: ["content-type"],
     methods: "*",
     exposedHeaders: "*",
     optionsSuccessStatus: 200,
